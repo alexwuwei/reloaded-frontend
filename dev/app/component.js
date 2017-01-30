e30f62ad12cbbe9da8247b00c0385498bb26e0d4
Hugo Guerra <guerrahugoa@gmail.com>
2017-01-29 18:58:58 -0800

    adds server and basic component files. adds webpack, inits node project

---
+++ dev/app/component.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = () => {
+  var element = document.createElement('h1');
+element.innerHTML = 'Hello world'; return element;
+}
---
+++ dev/app/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var component = require('./component');
+var app = document.createElement('div');
+document.body.appendChild(app);
+app.appendChild(component());
